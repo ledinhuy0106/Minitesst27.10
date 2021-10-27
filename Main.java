732ade64fbda82c3eb318d457b57823063754747
uyle1234 <91107969+uyle1234@users.noreply.github.com>
2021-10-27 10:00:38 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+import model.Pupil;
+import service.PubpilManager;
+
+public class Main {
+    public static void main(String[] args) {
+       PubpilManager pubpilManager= new PubpilManager();
+       Pupil pupil = new Pupil("Uy", 18 , 1, 7.0 , 6.4 , 8 );
+       Pupil pupil1 = new Pupil("Uy", 18 , 2 , 8, 6.4 , 8 );
+       Pupil pupil2 = new Pupil("Uy", 18 , 3, 2, 6 , 8 );
+       pubpilManager.add(pupil);
+       pubpilManager.add(pupil1);
+       pubpilManager.add(pupil2);
+       pubpilManager.print();
+       pubpilManager.deleteByCode(2);
+       pubpilManager.print();
+       pubpilManager.findIndexByid(1);
+       pubpilManager.print();
+       pubpilManager.sort();
+       pubpilManager.print();
+    }
+}
